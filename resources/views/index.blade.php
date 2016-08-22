ae803286999517cfbed0c2675c389a1d2440327a
alexa0503 <lori.w@live.cn>
2016-08-22 21:59:00 +0800

    页面 form qr

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,70 +1,70 @@
 @extends('layouts.app')
 @section('content')
 
-<div id="home_BG" style="width: 640px; height: 1038px; left: 0px; top: 0px;  background-image: url('/assets/images/home_BG.png');"></div>
-<div id="home_ge9" style="width: 166px; height: 166px; left: 439px; top: 824px;  background-image: url('/assets/images/home_ge9.png');"></div>
-<div id="home_ge8" style="width: 166px; height: 166px; left: 243px; top: 824px;  background-image: url('/assets/images/home_ge8.png');"></div>
-<div id="home_ge7" style="width: 165px; height: 166px; left: 44px; top: 824px;  background-image: url('/assets/images/home_ge7.png');"></div>
-<div id="home_ge6" style="width: 166px; height: 166px; left: 439px; top: 629px;  background-image: url('/assets/images/home_ge6.png');"></div>
-<div id="home_ge4" style="width: 165px; height: 166px; left: 44px; top: 629px;  background-image: url('/assets/images/home_ge4.png');"></div>
-<div id="home_ge3" style="width: 166px; height: 166px; left: 439px; top: 436px;  background-image: url('/assets/images/home_ge3.png');"></div>
-<div id="home_ge2" style="width: 166px; height: 166px; left: 243px; top: 436px;  background-image: url('/assets/images/home_ge2.png');"></div>
-<div id="home_ge1" style="width: 165px; height: 166px; left: 44px; top: 436px;  background-image: url('/assets/images/home_ge1.png');"></div>
-<div id="home_DianJiKaiShi1" style="width: 192px; height: 197px; left: 231px; top: 613px;  background-image: url('/assets/images/home_DianJiKaiShi1.png');"></div>
-<div id="home_DJKS2_SS" style="width: 192px; height: 197px; left: 231px; top: 613px;  background-image: url('/assets/images/home_DJKS2_SS.png');"></div>
-<div id="home_HuoDongGuiZe" style="width: 149px; height: 51px; left: 254px; top: 305px;  background-image: url('/assets/images/home_HuoDongGuiZe.png');"></div>
-<div id="home_WoYaoFenXiang" style="width: 150px; height: 51px; left: 408px; top: 305px;  background-image: url('/assets/images/home_WoYaoFenXiang.png');"></div>
-<div id="home_XuanKuang" style="width: 165px; height: 166px; left: 44px; top: 436px;  background-image: url('/assets/images/home_XuanKuang.png');"></div>
-<div id="GZ_layer_13" style="width: 640px; height: 1038px; left: 0px; top: 0px;  background-image: url('/assets/images/GZ_layer_13.png');"></div>
-<div id="GZ_DCBJ" style="width: 529px; height: 809px; left: 63px; top: 114px;  background-image: url('/assets/images/GZ_DCBJ.png');"></div>
+<div id="home_BG" style=" left: 0px; top: 0px;  "><img style="width: 640px; height: 1038px;" src="/assets/images/home_BG.png"/></div>
+<div id="home_ge9" style=" left: 439px; top: 824px;  "><img style="width: 166px; height: 166px;" src="/assets/images/home_ge9.png"/></div>
+<div id="home_ge8" style=" left: 243px; top: 824px;  "><img style="width: 166px; height: 166px;" src="/assets/images/home_ge8.png"/></div>
+<div id="home_ge7" style=" left: 44px; top: 824px;  "><img style="width: 165px; height: 166px;" src="/assets/images/home_ge7.png"/></div>
+<div id="home_ge6" style=" left: 439px; top: 629px;  "><img style="width: 166px; height: 166px;" src="/assets/images/home_ge6.png"/></div>
+<div id="home_ge4" style=" left: 44px; top: 629px;  "><img style="width: 165px; height: 166px;" src="/assets/images/home_ge4.png"/></div>
+<div id="home_ge3" style=" left: 439px; top: 436px;  "><img style="width: 166px; height: 166px;" src="/assets/images/home_ge3.png"/></div>
+<div id="home_ge2" style=" left: 243px; top: 436px;  "><img style="width: 166px; height: 166px;" src="/assets/images/home_ge2.png"/></div>
+<div id="home_ge1" style=" left: 44px; top: 436px;  "><img style="width: 165px; height: 166px;" src="/assets/images/home_ge1.png"/></div>
+<div id="home_DianJiKaiShi1" style=" left: 231px; top: 613px;  "><img style="width: 192px; height: 197px;" src="/assets/images/home_DianJiKaiShi1.png"/></div>
+<div id="home_DJKS2_SS" style=" left: 231px; top: 613px;  "><img style="width: 192px; height: 197px;" src="/assets/images/home_DJKS2_SS.png"/></div>
+<div id="home_HuoDongGuiZe" style=" left: 254px; top: 305px;  "><img style="width: 149px; height: 51px;" src="/assets/images/home_HuoDongGuiZe.png"/></div>
+<div id="home_WoYaoFenXiang" style=" left: 408px; top: 305px;  "><img style="width: 150px; height: 51px;" src="/assets/images/home_WoYaoFenXiang.png"/></div>
+<div id="home_XuanKuang" style=" left: 44px; top: 436px;  "><img style="width: 165px; height: 166px;" src="/assets/images/home_XuanKuang.png"/></div>
+<div id="GZ_layer_13" style=" left: 0px; top: 0px;  "><img style="width: 640px; height: 1038px;" src="/assets/images/GZ_layer_13.png"/></div>
+<div id="GZ_DCBJ" style=" left: 63px; top: 114px;  "><img style="width: 529px; height: 809px;" src="/assets/images/GZ_DCBJ.png"/></div>
 <div id="GZ_WenZi" style="width: 460px; height: 766px;  left: 106px; top: 143px; ">
 <img id="myScrollbar" style="width: 439px; height: 1200px; " src='/assets/images/GZ.png'/>
 </div>
-<div id="GZ_btnX" style="width: 60px; height: 60px; left: 557px; top: 88px;  background-image: url('/assets/images/GZ_btnX.png');"></div>
-<div id="YWL_layer_13" style="width: 640px; height: 1038px; left: 0px; top: 0px;  background-image: url('/assets/images/YWL_layer_13.png');"></div>
-<div id="YWL_MSG" style="width: 529px; height: 333px; left: 57px; top: 396px;  background-image: url('/assets/images/YWL_MSG.png');"></div>
-<div id="YWL_btnX" style="width: 60px; height: 60px; left: 554px; top: 369px;  background-image: url('/assets/images/YWL_btnX.png');"></div>
-<div id="YWL_HuoDongGuiZe" style="width: 204px; height: 70px; left: 223px; top: 634px;  background-image: url('/assets/images/YWL_HuoDongGuiZe.png');"></div>
-<div id="CYDD_MSK" style="width: 640px; height: 1038px; left: 0px; top: 0px;  background-image: url('/assets/images/CYDD_MSK.png');"></div>
-<div id="CYDD_MSG" style="width: 529px; height: 333px; left: 57px; top: 396px;  background-image: url('/assets/images/CYDD_MSG.png');"></div>
-<div id="CYDD_btnX" style="width: 60px; height: 60px; left: 554px; top: 369px;  background-image: url('/assets/images/CYDD_btnX.png');"></div>
-<div id="CYDD_HuoDongGuiZe" style="width: 204px; height: 70px; left: 119px; top: 620px;  background-image: url('/assets/images/CYDD_HuoDongGuiZe.png');"></div>
-<div id="CYDD_WoYaoFenXiang" style="width: 204px; height: 70px; left: 330px; top: 620px;  background-image: url('/assets/images/CYDD_WoYaoFenXiang.png');"></div>
-<div id="FORM_MSK" style="width: 640px; height: 1038px; left: 0px; top: 0px;  background-image: url('/assets/images/FORM_MSK.png');"></div>
-<div id="FORM_BG" style="width: 529px; height: 809px; left: 63px; top: 114px;  background-image: url('/assets/images/FORM_BG.png');"></div>
-<div id="FORM_submit" style="width: 131px; height: 45px; left: 265px; top: 557px;  background-image: url('/assets/images/FORM_submit.png');"></div>
-<div id="FORM_input_mobile" style="width: 239px; height: 33px; left: 246px; top: 501px;  background-image: url('/assets/images/FORM_input_mobile.png');"></div>
-<div id="FORM_input_name" style="width: 239px; height: 33px; left: 246px; top: 444px;  background-image: url('/assets/images/FORM_input_name.png');"></div>
-<div id="FORM_qrcode" style="width: 134px; height: 133px; left: 120px; top: 737px;  ">
-	<img style="width: 134px; height: 134px; padding:100px;margin:-100px;" src="/assets/images/FORM_qrcode.jpg"></div>
-<div id="FORM_btnX" style="width: 60px; height: 60px; left: 557px; top: 88px;  background-image: url('/assets/images/FORM_btnX.png');"></div>
-<div id="WIN_layer_13" style="width: 640px; height: 1038px; left: 0px; top: 0px;  background-image: url('/assets/images/WIN_layer_13.png');"></div>
-<div id="WIN_BG" style="width: 529px; height: 333px; left: 57px; top: 396px;  background-image: url('/assets/images/WIN_BG.png');"></div>
-<div id="WIN_btnX" style="width: 60px; height: 60px; left: 554px; top: 369px;  background-image: url('/assets/images/WIN_btnX.png');"></div>
-<div id="WIN_btnShare" style="width: 204px; height: 70px; left: 223px; top: 634px;  background-image: url('/assets/images/WIN_btnShare.png');"></div>
-<div id="share_msk" style="width: 640px; height: 1038px; left: 0px; top: 0px;  background-image: url('/assets/images/share_msk.png');"></div>
-<div id="share_obj_d6" style="width: 79px; height: 79px; left: 198px; top: 464px;  background-image: url('/assets/images/share_obj_d6.png');"></div>
-<div id="share_obj_d6_KaoBei_11" style="width: 79px; height: 79px; left: 311px; top: 325px;  background-image: url('/assets/images/share_obj_d6_KaoBei_11.png');"></div>
-<div id="share_obj_d6_KaoBei_12" style="width: 79px; height: 79px; left: 322px; top: 429px;  background-image: url('/assets/images/share_obj_d6_KaoBei_12.png');"></div>
-<div id="share_obj_d6_KaoBei" style="width: 79px; height: 79px; left: 441px; top: 288px;  background-image: url('/assets/images/share_obj_d6_KaoBei.png');"></div>
-<div id="share_obj_d6_KaoBei_2" style="width: 79px; height: 79px; left: 415px; top: 350px;  background-image: url('/assets/images/share_obj_d6_KaoBei_2.png');"></div>
-<div id="share_obj_d6_KaoBei_3" style="width: 61px; height: 61px; left: 350px; top: 359px;  background-image: url('/assets/images/share_obj_d6_KaoBei_3.png');"></div>
-<div id="share_obj_d6_KaoBei_4" style="width: 47px; height: 47px; left: 398px; top: 357px;  background-image: url('/assets/images/share_obj_d6_KaoBei_4.png');"></div>
-<div id="share_obj_d6_KaoBei_5" style="width: 38px; height: 38px; left: 399px; top: 432px;  background-image: url('/assets/images/share_obj_d6_KaoBei_5.png');"></div>
-<div id="share_obj_d6_KaoBei_6" style="width: 38px; height: 38px; left: 368px; top: 476px;  background-image: url('/assets/images/share_obj_d6_KaoBei_6.png');"></div>
-<div id="share_obj_d6_KaoBei_7" style="width: 38px; height: 38px; left: 328px; top: 470px;  background-image: url('/assets/images/share_obj_d6_KaoBei_7.png');"></div>
-<div id="share_obj_d6_KaoBei_8" style="width: 38px; height: 38px; left: 407px; top: 278px;  background-image: url('/assets/images/share_obj_d6_KaoBei_8.png');"></div>
-<div id="share_obj_d6_KaoBei_9" style="width: 38px; height: 38px; left: 520px; top: 225px;  background-image: url('/assets/images/share_obj_d6_KaoBei_9.png');"></div>
-<div id="share_obj_d6_KaoBei_10" style="width: 38px; height: 38px; left: 445px; top: 225px;  background-image: url('/assets/images/share_obj_d6_KaoBei_10.png');"></div>
-<div id="share_obj_DJYSJ_FXZP" style="width: 272px; height: 68px; left: 192px; top: 672px;  background-image: url('/assets/images/share_obj_DJYSJ_FXZP.png');"></div>
-<div id="share_obj_Zu_15" style="width: 183px; height: 153px; left: 258px; top: 503px;  background-image: url('/assets/images/share_obj_Zu_15.png');"></div>
-<div id="share_obj_layer_18" style="width: 160px; height: 169px; left: 237px; top: 470px;  background-image: url('/assets/images/share_obj_layer_18.png');"></div>
-<div id="share_obj_layer_18_KaoBei" style="width: 98px; height: 97px; left: 279px; top: 432px;  background-image: url('/assets/images/share_obj_layer_18_KaoBei.png');"></div>
-<div id="share_obj_layer_18_KB_3" style="width: 40px; height: 42px; left: 361px; top: 456px;  background-image: url('/assets/images/share_obj_layer_18_KB_3.png');"></div>
-<div id="share_obj_layer_18_KB_2" style="width: 52px; height: 54px; left: 368px; top: 410px;  background-image: url('/assets/images/share_obj_layer_18_KB_2.png');"></div>
-<div id="share_obj_layer_18_KB_4" style="width: 35px; height: 36px; left: 481px; top: 326px;  background-image: url('/assets/images/share_obj_layer_18_KB_4.png');"></div>
-<div id="share_obj_layer_18_KB_5" style="width: 21px; height: 22px; left: 431px; top: 344px;  background-image: url('/assets/images/share_obj_layer_18_KB_5.png');"></div>
-<div id="share_obj_layer_18_KB_6" style="width: 21px; height: 22px; left: 520px; top: 173px;  background-image: url('/assets/images/share_obj_layer_18_KB_6.png');"></div>
+<div id="GZ_btnX" style=" left: 557px; top: 88px;  "><img style="width: 60px; height: 60px;" src="/assets/images/GZ_btnX.png"/></div>
+<div id="YWL_layer_13" style=" left: 0px; top: 0px;  "><img style="width: 640px; height: 1038px;" src="/assets/images/YWL_layer_13.png"/></div>
+<div id="YWL_MSG" style=" left: 57px; top: 396px;  "><img style="width: 529px; height: 333px;" src="/assets/images/YWL_MSG.png"/></div>
+<div id="YWL_btnX" style=" left: 554px; top: 369px;  "><img style="width: 60px; height: 60px;" src="/assets/images/YWL_btnX.png"/></div>
+<div id="YWL_HuoDongGuiZe" style=" left: 223px; top: 634px;  "><img style="width: 204px; height: 70px;" src="/assets/images/YWL_HuoDongGuiZe.png"/></div>
+<div id="CYDD_MSK" style=" left: 0px; top: 0px;  "><img style="width: 640px; height: 1038px;" src="/assets/images/CYDD_MSK.png"/></div>
+<div id="CYDD_MSG" style=" left: 57px; top: 396px;  "><img style="width: 529px; height: 333px;" src="/assets/images/CYDD_MSG.png"/></div>
+<div id="CYDD_btnX" style=" left: 554px; top: 369px;  "><img style="width: 60px; height: 60px;" src="/assets/images/CYDD_btnX.png"/></div>
+<div id="CYDD_HuoDongGuiZe" style=" left: 119px; top: 620px;  "><img style="width: 204px; height: 70px;" src="/assets/images/CYDD_HuoDongGuiZe.png"/></div>
+<div id="CYDD_WoYaoFenXiang" style=" left: 330px; top: 620px;  "><img style="width: 204px; height: 70px;" src="/assets/images/CYDD_WoYaoFenXiang.png"/></div>
+<div id="FORM_MSK" style=" left: 0px; top: 0px;  "><img style="width: 640px; height: 1038px;" src="/assets/images/FORM_MSK.png"/></div>
+<div id="FORM_BG" style=" left: 63px; top: 114px;  "><img style="width: 529px; height: 809px;" src="/assets/images/FORM_BG.png"/></div>
+<div id="FORM_submit" style=" left: 265px; top: 557px;  "><img style="width: 131px; height: 45px;" src="/assets/images/FORM_submit.png"/></div>
+<div id="FORM_input_mobile" style=" left: 246px; top: 501px;  "><img style="width: 239px; height: 33px;" src="/assets/images/FORM_input_mobile.png"/></div>
+<div id="FORM_input_name" style=" left: 246px; top: 444px;  "><img style="width: 239px; height: 33px;" src="/assets/images/FORM_input_name.png"/></div>
+<div id="FORM_qrcode" style=" left: 120px; top: 737px;  "><img style="padding:100px;margin:-100px;width: 134px; height: 133px;" src="/assets/images/FORM_qrcode.png"/></div>
+<div id="FORM_btnX" style=" left: 557px; top: 88px;  "><img style="width: 60px; height: 60px;" src="/assets/images/FORM_btnX.png"/></div>
+<div id="WIN_layer_13" style=" left: 0px; top: 0px;  "><img style="width: 640px; height: 1038px;" src="/assets/images/WIN_layer_13.png"/></div>
+<div id="WIN_BG" style=" left: 57px; top: 396px;  "><img style="width: 529px; height: 333px;" src="/assets/images/WIN_BG.png"/></div>
+<div id="WIN_btnX" style=" left: 554px; top: 369px;  "><img style="width: 60px; height: 60px;" src="/assets/images/WIN_btnX.png"/></div>
+<div id="WIN_btnShare" style=" left: 223px; top: 634px;  "><img style="width: 204px; height: 70px;" src="/assets/images/WIN_btnShare.png"/></div>
+<div id="share_msk" style=" left: 0px; top: 0px;  "><img style="width: 640px; height: 1038px;" src="/assets/images/share_msk.png"/></div>
+<div id="share_obj_d6" style=" left: 198px; top: 464px;  "><img style="width: 79px; height: 79px;" src="/assets/images/share_obj_d6.png"/></div>
+<div id="share_obj_d6_KaoBei_11" style=" left: 311px; top: 325px;  "><img style="width: 79px; height: 79px;" src="/assets/images/share_obj_d6_KaoBei_11.png"/></div>
+<div id="share_obj_d6_KaoBei_12" style=" left: 322px; top: 429px;  "><img style="width: 79px; height: 79px;" src="/assets/images/share_obj_d6_KaoBei_12.png"/></div>
+<div id="share_obj_d6_KaoBei" style=" left: 441px; top: 288px;  "><img style="width: 79px; height: 79px;" src="/assets/images/share_obj_d6_KaoBei.png"/></div>
+<div id="share_obj_d6_KaoBei_2" style=" left: 415px; top: 350px;  "><img style="width: 79px; height: 79px;" src="/assets/images/share_obj_d6_KaoBei_2.png"/></div>
+<div id="share_obj_d6_KaoBei_3" style=" left: 350px; top: 359px;  "><img style="width: 61px; height: 61px;" src="/assets/images/share_obj_d6_KaoBei_3.png"/></div>
+<div id="share_obj_d6_KaoBei_4" style=" left: 398px; top: 357px;  "><img style="width: 47px; height: 47px;" src="/assets/images/share_obj_d6_KaoBei_4.png"/></div>
+<div id="share_obj_d6_KaoBei_5" style=" left: 399px; top: 432px;  "><img style="width: 38px; height: 38px;" src="/assets/images/share_obj_d6_KaoBei_5.png"/></div>
+<div id="share_obj_d6_KaoBei_6" style=" left: 368px; top: 476px;  "><img style="width: 38px; height: 38px;" src="/assets/images/share_obj_d6_KaoBei_6.png"/></div>
+<div id="share_obj_d6_KaoBei_7" style=" left: 328px; top: 470px;  "><img style="width: 38px; height: 38px;" src="/assets/images/share_obj_d6_KaoBei_7.png"/></div>
+<div id="share_obj_d6_KaoBei_8" style=" left: 407px; top: 278px;  "><img style="width: 38px; height: 38px;" src="/assets/images/share_obj_d6_KaoBei_8.png"/></div>
+<div id="share_obj_d6_KaoBei_9" style=" left: 520px; top: 225px;  "><img style="width: 38px; height: 38px;" src="/assets/images/share_obj_d6_KaoBei_9.png"/></div>
+<div id="share_obj_d6_KaoBei_10" style=" left: 445px; top: 225px;  "><img style="width: 38px; height: 38px;" src="/assets/images/share_obj_d6_KaoBei_10.png"/></div>
+<div id="share_obj_DJYSJ_FXZP" style=" left: 192px; top: 672px;  "><img style="width: 272px; height: 68px;" src="/assets/images/share_obj_DJYSJ_FXZP.png"/></div>
+<div id="share_obj_Zu_15" style=" left: 258px; top: 503px;  "><img style="width: 183px; height: 153px;" src="/assets/images/share_obj_Zu_15.png"/></div>
+<div id="share_obj_layer_18" style=" left: 237px; top: 470px;  "><img style="width: 160px; height: 169px;" src="/assets/images/share_obj_layer_18.png"/></div>
+<div id="share_obj_layer_18_KaoBei" style=" left: 279px; top: 432px;  "><img style="width: 98px; height: 97px;" src="/assets/images/share_obj_layer_18_KaoBei.png"/></div>
+<div id="share_obj_layer_18_KB_3" style=" left: 361px; top: 456px;  "><img style="width: 40px; height: 42px;" src="/assets/images/share_obj_layer_18_KB_3.png"/></div>
+<div id="share_obj_layer_18_KB_2" style=" left: 368px; top: 410px;  "><img style="width: 52px; height: 54px;" src="/assets/images/share_obj_layer_18_KB_2.png"/></div>
+<div id="share_obj_layer_18_KB_4" style=" left: 481px; top: 326px;  "><img style="width: 35px; height: 36px;" src="/assets/images/share_obj_layer_18_KB_4.png"/></div>
+<div id="share_obj_layer_18_KB_5" style=" left: 431px; top: 344px;  "><img style="width: 21px; height: 22px;" src="/assets/images/share_obj_layer_18_KB_5.png"/></div>
+<div id="share_obj_layer_18_KB_6" style=" left: 520px; top: 173px;  "><img style="width: 21px; height: 22px;" src="/assets/images/share_obj_layer_18_KB_6.png"/></div>
+
 <script src="/assets/js/WJpreloader.js"></script>
 {{ csrf_field() }}
 @endsection
